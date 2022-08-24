02a4da991d8c5626c5b288c27cc04d47b4fc51b4
elyssa97 <81210842+elyssa97@users.noreply.github.com>
2022-08-24 18:54:56 +0800

    Delete easyparcel directory

--- easyparcel/easyparcel_shipping_ns/models/sale_order.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import api, fields, models
-
-
-class EasyParcelSaleOrder(models.Model):
-    _inherit = 'sale.order'
-
-    easyparcel_service = fields.Char(copy=False, string='EasyParcel Service')
-
--- easyparcel/shipping_backbone_ns/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import stock_picking
-from . import delivery_carrier
-from . import shipping_backbone_ns
-from . import carrier_multi_packages_ns
